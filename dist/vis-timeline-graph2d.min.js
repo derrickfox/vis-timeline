78bc741251ecf6ad159949f1aeeab6413e66af10
Me <fox.derrick@gmail.com>
2019-10-08 20:26:34 -0400

    saving

--- dist/vis-timeline-graph2d.min.js
+++ dist/vis-timeline-graph2d.min.js
@@ -5,7 +5,7 @@
  * Create a fully customizable, interactive timeline with items and ranges.
  *
  * @version 0.0.0-no-version
- * @date    2019-10-08T23:17:22Z
+ * @date    2019-10-08T23:39:59Z
  *
  * @copyright (c) 2011-2017 Almende B.V, http://almende.com
  * @copyright (c) 2018-2019 visjs contributors, https://github.com/visjs
